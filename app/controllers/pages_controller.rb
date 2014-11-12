a497aa86be9f3f3d31149005d8e8c69446ab12a3
nicholasnadel <nicholasnadel@gmail.com>
2014-11-12 13:14:20 -0800

    Implemented update pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,33 @@ class PagesController < ApplicationController
   end
 
   def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(params.require(:page).permit(:title, :body))
+    if @page.save
+      flash[:notice] = "Page was saved."
+      redirect_to @page
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
-end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update_attributes(params.require(:page).permit(:title, :body))
+           flash[:notice] = "Page was updated."
+           redirect_to @page
+         else
+           flash[:error] = "There was an error saving the post. Please try again."
+           render :edit
+         end
+       end
+
+  end
